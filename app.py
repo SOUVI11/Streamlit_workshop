af917493b6640de4ab49f70d459db83b32ad8318
VISHNU <91796032+SOUVI11@users.noreply.github.com>
2022-05-16 18:23:15 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,11 @@
+import streamlit as st
+st.title('Hello world')
+st.header('This is a header')
+st.subheader('This is a sub header')
+st.text('Hello again') #add a text
+st.success('Success') #shows success message
+st.info('Information') 
+st.warning('Warning')
+st.error('Error')
+st.write('Text with write')
+st.write(range(10))
